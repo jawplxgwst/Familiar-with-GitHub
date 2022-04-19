8e4bf73665aa02a83b7f47c11a60cb321c64189f
jawplxgwst <99839875+jawplxgwst@users.noreply.github.com>
2022-04-19 17:34:12 -0400

    Quick Update
    Changed the y variable to s
    added another y variable that prints at the end of the process

--- iterativeprocess.java
+++ iterativeprocess.java
@@ -8,13 +8,15 @@
 public class iterativeprocess {
     public static void main (String[] args){
 
-        double y = 0;
+        double s = 0;
         double x = 2;
+        double y = 4;
 
         for (int i = 0; i<10; i++) {
             System.out.println(x);
-            y = (x * x) - 2;
-            x = y;
+            s = (x * x) - 2;
+            x = s;
         }
+        System.out.print(y);
     }
 }
